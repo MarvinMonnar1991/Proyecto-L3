d20cf4491dbc1cfeaa4ffe12eb69b8ce236b1bde
MarvinMonnar1991 <monnarmarvin@gmail.com>
2021-03-05 15:27:12 -0600

    Entrega 5

---
+++ ProyectoL3-main/Entregas 5/Proyecto-L3-main/Entregas/BL.Entregas/Contexto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Entregas
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(): base("Clientes")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            Database.SetInitializer(new DatosdeInicio());//
+        }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Tipo> Tipos { get; set; }//
+        
+    }
+
+}
